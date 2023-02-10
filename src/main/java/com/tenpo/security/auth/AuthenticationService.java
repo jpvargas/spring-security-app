4965742d15faef75b0f383497a40d58b9c2f80f0
jpvargas <juanpablo.vargas@frubana.com>
2023-02-10 15:29:09 -0300

    Add logic to Authentication request.

---
+++ src/main/java/com/tenpo/security/auth/AuthenticationService.java
@@ -0,0 +1,58 @@
+package com.tenpo.security.auth;
+
+import com.tenpo.security.config.JwtService;
+import com.tenpo.security.user.Role;
+import com.tenpo.security.user.User;
+import com.tenpo.security.user.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+  private final UserRepository repository;
+
+  private final PasswordEncoder passwordEncoder;
+
+  private final JwtService jwtService;
+
+  private final AuthenticationManager authenticationManager;
+
+  public AuthenticationResponse register(RegisterRequest request) {
+    var user = User.builder()
+        .firstname(request.getFirstname())
+        .lastname(request.getLastname())
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .role(Role.USER)
+        .build();
+    repository.save(user);
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
+  }
+
+  public AuthenticationResponse authenticate(AuthenticateRequest request) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getEmail(),
+            request.getPassword()
+        )
+    );
+    var user = repository.findByEmail(request.getEmail())
+        .orElseThrow();
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
+  }
+}
--- src/main/java/com/tenpo/security/config/SecurityConfiguration.java
+++ src/main/java/com/tenpo/security/config/SecurityConfiguration.java
@@ -3,15 +3,21 @@ package com.tenpo.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+
+  private final AuthenticationProvider authenticationProvider;
+
+  private final JwtAuthenticationFilter jwtAuthFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,7 +25,7 @@ public class SecurityConfiguration {
         .csrf()
         .disable()
         .authorizeHttpRequests()
-        .requestMatchers("")
+        .requestMatchers("/api/v1/auth/**")
         .permitAll()
         .anyRequest()
         .authenticated()
@@ -28,8 +34,7 @@ public class SecurityConfiguration {
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authenticationProvider(authenticationProvider)
-        .addFilterBefore()
-
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
